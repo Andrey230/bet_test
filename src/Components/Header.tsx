dfe0e6230807a409bf42ec1a0c4883cb8f006621
a.bilous <a.bilous@softum.io>
2024-02-19 10:53:10 +0100

    fix link

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -65,7 +65,7 @@ export default function Header() {
                 <div className="flex-none">
                     {connected ? (
                         <>
-                            <NavLink to="event/create">
+                            <NavLink to="/event/create">
                                 <div className="btn btn-ghost btn-circle">
                                     <div className="indicator">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" stroke="currentColor" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
